cdc2f035816c8eaf6a8ba742d5632ac71b06d010
Alesiya19 <73656406+Alesiya19@users.noreply.github.com>
2020-11-11 13:39:58 +0300

    Add files via upload

---
+++ Animal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2.animal
+{
+   abstract class Animal
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Vid { get; set; }
+        public string Сapability { get; set; }
+        public Animal(string name, int age, string vid, string capability)
+        {
+            Name = name;
+            Age = age;
+            Vid = vid;
+            Сapability = capability;
+        }
+        public override string ToString()
+        {
+            return String.Format($"{"Вид: "}{Vid} {", Имя: "} { Name}{", Возраст: "} { Age}{", Способности: "} {Сapability}");
+        }
+    }
+}
---
+++ pigs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2.animal
+{
+    class pigs:Animal
+    {
+        public pigs(string name, int age, string vid, string capability) : base(name, age, vid, capability)
+        {
+        }
+    }
+}
